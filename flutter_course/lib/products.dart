a410b6af71a47b9e2be996901d0dfcbedc21b5e8
Arnaud Fosso Pouangue <arnaud.fossop@gmail.com>
2018-08-14 08:24:16 -0400

    Rendering elements on demand

--- flutter_course/lib/products.dart
+++ flutter_course/lib/products.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-
   final List<String> products;
 
   Products(this.products);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-              children: products
-                  .map((element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/food.jpg'),
-                            Text(element)
-                          ],
-                        ),
-                      ))
-                  .toList(),
-            );
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
+  }
 }
