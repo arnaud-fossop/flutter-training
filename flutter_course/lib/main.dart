4f1b014c7a2fdae53dd54752b4fb8223ca7640c7
Arnaud Fosso Pouangue <arnaud.fossop@gmail.com>
2018-08-14 08:13:53 -0400

    First use of ListView

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
 void main() {
-  //Does not work in my environment, instead go to Command Palette and search for "Toggle Debug Painting"
-  //https://www.udemy.com/learn-flutter-dart-to-build-ios-android-apps/learn/v4/questions/4592926
-  debugPaintSizeEnabled = true;
-  debugPaintBaselinesEnabled = true;
-  debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
 
@@ -16,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowMaterialGrid: true,
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.deepOrange,
