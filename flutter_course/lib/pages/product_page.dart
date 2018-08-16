b1bb8b050902d80810c3c5265c7217307476f8ce
Arnaud Fosso Pouangue <arnaud.fossop@gmail.com>
2018-08-16 07:13:12 -0400

    Passing data through navigation

--- flutter_course/lib/pages/product_page.dart
+++ flutter_course/lib/pages/product_page.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product details'),
+        title: Text(title + ' details'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/food.jpg'),
+            Image.asset(imageUrl),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text('Details'),
+              child: Text(title),
             ),
             Container(
               padding: EdgeInsets.all(10.0),
--- flutter_course/lib/products.dart
+++ flutter_course/lib/products.dart
@@ -23,7 +23,7 @@ class Products extends StatelessWidget {
                 onPressed: () => Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (BuildContext context) => ProductPage(),
+                        builder: (BuildContext context) => ProductPage(products[index]['title'], products[index]['image']),
                       ),
                     ),
               )
