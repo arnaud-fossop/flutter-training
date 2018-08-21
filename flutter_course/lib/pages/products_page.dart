6a52199508e6bb00e0f25664529135952de7e542
Arnaud Fosso Pouangue <arnaud.fossop@gmail.com>
2018-08-21 06:08:27 -0400

    Version with navigation issues

--- flutter_course/lib/pages/products_page.dart
+++ flutter_course/lib/pages/products_page.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 
-import '../product_manager.dart';
+//import '../product_manager.dart';
+import './products_management_page.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Easy List'),
-      ),
-      body: ProductManager(),
-    );
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('Manage Products'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        ProductsManagementPage();
+                      },
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Easy List'),
+        ),
+        body: Container(
+          child: RaisedButton(
+            child: Text('Essai'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (BuildContext context) {
+                  ProductsManagementPage();
+                }),
+              );
+            },
+          ),
+        )
+        // ProductManager(),
+        );
   }
 }
